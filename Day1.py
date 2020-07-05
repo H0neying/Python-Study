b5e7f4e4a3f50e7ba42e3c83c179802d45a909d3
H0neying <60545156+H0neying@users.noreply.github.com>
2020-07-05 14:03:37 +0900

    Add files via upload
    0705 pre-study

---
+++ Day1.py
@@ -0,0 +1,24 @@
+#This code has been written by Jongheon Yeo(H0neying) @SNU ECE
+#If you want to contact me, please send an email to zkdnqh12@snu.ac.kr
+
+#Day 1. Introduction to CS, string, int, float and console I/O
+
+############
+#0. Python?#
+############
+
+####################
+#1. String Variable#
+####################
+
+################################################
+#2. int and float variables & their computation#
+################################################
+
+#########################
+#3. Console Input/Output#
+#########################
+
+
+
+
